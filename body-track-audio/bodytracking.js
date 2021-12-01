0e860057feb917e0e4fc7dc1c20b605e383e007b
franciscorafart <francisco.rafart@gmail.com>
2021-12-01 11:16:09 -0500

    Separate body tracking code

---
+++ body-track-audio/bodytracking.js
@@ -0,0 +1,138 @@
+
+import { setAudio, mapPositionToSoundParams } from './audio-utils.js'
+import { drawKeypoints, drawSkeleton, getBodyParts } from './utils.js';
+
+const videoWidth = window.innerWidth;
+const videoHeight = window.innerHeight;
+
+let net;
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+export async function initBodyTracking(sounds, audioCtx) {
+    net = await posenet.load();
+
+    // Better accuracy model / slower to load
+    // net = await posenet.load({
+    //     architecture: 'ResNet50',
+    //     outputStride: 32,
+    //     inputResolution: { width: 640, height:480 },
+    //     quantBytes: 2,
+    // });
+
+    let video;
+
+    try {
+        video = await loadVideo();
+    } catch (e) {
+        throw e;
+    }
+
+    detectPoseInRealTime(video, net, sounds, audioCtx);
+}
+
+async function loadVideo() {
+    const video = await setupCamera();
+    video.play();
+    return video;
+}
+
+async function setupCamera(){
+    if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        throw new Error(
+            'Browser API navigator.mediaDevices.getUserMedia not available'
+        )
+    }
+
+    const video = document.getElementById('video');
+    video.width = videoWidth;
+    video.height = videoHeight;
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+            facingMode: 'user',
+            width: videoWidth,
+            height: videoHeight,
+        },
+    });
+
+    video.srcObject = stream;
+
+    return new Promise(
+        resolve => (video.onloadedmetadata = () =>
+        resolve(video))
+    );
+}
+
+const resetCanvas = (ctx) => {
+    ctx.clearRect(0,0, videoWidth, videoHeight);
+    ctx.save();
+    ctx.scale(-1, 1);
+    ctx.translate(-videoWidth, 0);
+    ctx.drawImage(video, 0, 0, window.innerWidth, window.innerHeight);
+    ctx.restore();
+}
+
+async function poseDetectionFrame(video, net, ctx, sounds, audioCtx, flipPoseHorizontal) {
+    const poses = await net.estimateMultiplePoses(video, {
+        flipHorizontal: flipPoseHorizontal,
+        scroreThreshold: 0.7,
+    });
+
+    const minPoseConfidence = 0.5;
+    const minPartConfidence = 0.5;
+
+    resetCanvas(ctx);
+
+    for (const [idx, pose] of poses.entries()) {
+        const bodyPartPositions = getBodyParts(pose.keypoints, minPoseConfidence, videoHeight, videoWidth);
+
+        // Draw tracking figure
+        drawKeypoints(pose.keypoints, minPoseConfidence, ctx);
+        drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+
+        // TODO: Set visuals.
+
+        // Set sounds.
+        if (sounds && sounds[idx]) {
+            const fxPositions = mapPositionToSoundParams(
+                bodyPartPositions['nose'].x, //pan
+                bodyPartPositions['nose'].y, //gain
+                bodyPartPositions['leftWrist'].x, //crossSynthesis
+                bodyPartPositions['leftWrist'].y, //distortion
+                bodyPartPositions['rightWrist'].x, //feedback
+                bodyPartPositions['rightWrist'].y, //reverb
+            )
+            setAudio(fxPositions, audioCtx, sounds[idx]);
+        }
+    }
+
+    requestAnimationFrame(() => poseDetectionFrame(
+        video,
+        net,
+        ctx,
+        sounds,
+        audioCtx,
+        flipPoseHorizontal,
+    ));
+}
+
+function detectPoseInRealTime(video, net, sounds, audioCtx) {
+    const canvas = document.getElementById('output');
+    const ctx = canvas.getContext('2d');
+
+    const flipPoseHorizontal = true;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // Draw video pixels on canvas, draw keypoints, and set audio state
+    poseDetectionFrame(
+        video, 
+        net, 
+        ctx, 
+        sounds, 
+        audioCtx, 
+        flipPoseHorizontal,
+    );
+}
