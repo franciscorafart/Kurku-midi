a083cfc028395f612558e0302dcfcc33970fbf0d
franciscorafart <francisco.rafart@gmail.com>
2022-04-16 16:17:22 -0400

    Improve FX boxes

--- soma/front-end/src/components/AudioFXPanel.tsx
+++ soma/front-end/src/components/AudioFXPanel.tsx
@@ -6,19 +6,32 @@ import selectedEffect from 'atoms/selectedEffect';
 const Container = styled.div`
     display: flex;
     flex: wrap;
-    justify-content: space-between;
-    gap: 20px;
+    padding: 20px;
+    // justify-content: space-between;
     min-height: 300px;
     border: 1px solid black;
 `;
 
-const EffecContainer = styled.div<{selected: boolean}>`
+const EffectConnect = styled.div`
+    display: flex;
+`
+const EffecContainer = styled.div<{selected: boolean, selectable?: boolean}>`
+    display: flex;
     height: 80px;
     width: 160px;
+    align-items: center;
+    justify-content: center;
     border: 1px solid ${({ selected }) => selected ? 'red' : 'blue'};
     border-radius: 5px;
-    cursor: pointer;
+    cursor: ${({ selectable }) => selectable ? 'pointer' : 'auto'};
 `;
+const Cable = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80px;
+    width: 40px;
+`
 
 function AudioFXPanel() {
     const sessionCfg = useRecoilValue(sessionConfig);
@@ -28,12 +41,19 @@ function AudioFXPanel() {
     return (
         <Container>
             {sessionCfg.effects.map(
-                eff => <EffecContainer 
-                    selected={eff.key === selected.key && eff.bodyPart === selected.bodyPart}
-                    onClick={() => setSelected({ key: eff.key, bodyPart: eff.bodyPart})} 
-                    key={`${eff.key}-${eff.bodyPart}`}
-                >{eff.key}-{eff.bodyPart}
-                </EffecContainer>)}
+                eff => 
+                <EffectConnect>
+                    <EffecContainer 
+                        selected={eff.key === selected.key && eff.bodyPart === selected.bodyPart}
+                        onClick={() => setSelected({ key: eff.key, bodyPart: eff.bodyPart})} 
+                        key={`${eff.key}-${eff.bodyPart}`}
+                        selectable
+                        >{eff.key}-{eff.bodyPart}
+                    </EffecContainer>
+                    <Cable>{`==>`}</Cable>
+                </EffectConnect>)
+            }
+            <EffecContainer selected={false}>Master</EffecContainer>
         </Container>
     );
 };
