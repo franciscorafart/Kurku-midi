2a8aeef9f8cd213a9178c391eb8a194ecf01101e
franciscorafart <francisco.rafart@gmail.com>
2022-12-10 16:06:23 -0500

    Remove body tracking panel and change styled components install

--- client/types/asset.d.ts
+++ client/types/asset.d.ts
@@ -3,13 +3,3 @@ declare module "*.wav" {
   const value: any;
   export default value;
 }
-
-declare module "styled-components" {
-  const value: any;
-  export default value;
-}
-
-// declare module "lodash" {
-//   const value: any;
-//   export default value;
-// }
