c3a19316beff5cd2ef3bc4c29161c0f382795ef4
franciscorafart <francisco.rafart@gmail.com>
2022-12-24 20:53:46 -0500

    Service worker logs

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -26,9 +26,22 @@ type Config = {
 };
 
 export function register(config?: Config) {
+  console.log("in register", process.env.NODE_ENV);
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     // The URL constructor is available in all browsers that support SW.
+    console.log({
+      PUBLIC_URL: process.env.PUBLIC_URL,
+      wLocation: window.location.href,
+    });
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    console.log(
+      "publicUrl",
+      publicUrl,
+      "window.location.origin",
+      window.location.origin,
+      "isLocalHost",
+      isLocalhost
+    );
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
