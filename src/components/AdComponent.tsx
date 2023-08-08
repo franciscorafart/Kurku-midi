eb140fdfd01e0aa5174909ecba4ea9dcaea39921
Francisco Rafart <francisco.rafart@gmail.com>
2023-08-07 20:37:54 -0400

    Remove ad if paid user

--- src/components/AdComponent.tsx
+++ src/components/AdComponent.tsx
@@ -1,30 +1,51 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useRef, useState } from "react";
 import { User } from "context";
 
 const Ad = () => {
   const isPaidUser = useContext(User);
+  const adRef = useRef<HTMLElement | null>(null);
+  const adScriptRef = useRef<HTMLScriptElement | null>(null);
+  const [adLoaded, setAdLoaded] = useState(false);
 
   useEffect(() => {
-    if (!isPaidUser) {
-      const script = document.createElement("script");
+    const script = document.createElement("script");
 
-      script.src = "//ophoacit.com/1?z=5884602";
-      script.async = true;
-      script.id = "adScript";
-      document.head.appendChild(script);
-    } else {
-      // Find ad and remove ad and script
-      const ad = document.getElementById("p_5884602");
-      const adScript = document.getElementById("adScript");
+    script.src = "//ophoacit.com/1?z=5884602";
+    script.async = true;
+    script.id = "adScript";
+    document.head.appendChild(script);
+    adScriptRef.current = script;
 
-      if (ad) {
-        ad.remove();
-      }
-      if (adScript) {
-        adScript.remove();
-      }
+    script.onload = () => {
+      // The script has loaded, so start observing for the dynamic element
+      const observer = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) {
+          // Check if the dynamic element has been added to the DOM
+          if (mutation.type === "childList") {
+            const dynamicElement = document.getElementById("p_5884602");
+
+            if (dynamicElement) {
+              adRef.current = dynamicElement;
+              setAdLoaded(true);
+              // Stop observing once the element is found
+              observer.disconnect();
+            }
+          }
+        }
+      });
+
+      observer.observe(document.body, { childList: true, subtree: true });
+    };
+
+    document.body.appendChild(script);
+  }, []);
+
+  useEffect(() => {
+    if (adLoaded && isPaidUser) {
+      adRef?.current?.remove();
+      adScriptRef?.current?.remove();
     }
-  }, [isPaidUser]);
+  }, [adLoaded, isPaidUser]);
 
   return <div></div>;
 };
